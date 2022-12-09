0542d3b1a0d9246c196aa575785264da0e15a928
dmychel <dmychel@tutanota.com>
2022-12-09 10:41:55 -0500

    I have added multiple color options to pick from aswell as an eraser(which is just the color white). I made it so once the user chooses the size of their canvas the click me button disapears so they cannot brick the layout of the grid. When the user clicks the restart button it will refresh the page and the click me button will reappear

--- main.js
+++ main.js
@@ -1,49 +1,136 @@
 console.log('hello world')
 
 const container = document.getElementById('container_div');
+const gridButton = document.getElementById('choice');
 
 let newDiv;
 let userSelection;
 
 
-function createGrid(div){
-    for(let i = 0; i < div; i++){
+function createGrid(number){
+    let numSquare = number * number;
+    console.log(number)
+    console.log(numSquare)
+    for(let i = 0; i < numSquare; i++){
         newDiv = document.createElement('div');
         newDiv.className = 'new-div'
         newDiv.id = 'newDiv'
         container.appendChild(newDiv)
     }
-}
-createGrid(256);
-
-const gridDiv = document.querySelectorAll('#newDiv');
-gridDiv.forEach(div =>{
+    container.style.setProperty('--grid-cols', number);
+    container.style.setProperty('--grid-rows', number);
+    
+    const gridDiv = document.querySelectorAll('#newDiv');
+    gridDiv.forEach(div =>{
     div.addEventListener('mouseover', e => {
-    div.style.backgroundColor = 'blue'
+    div.style.backgroundColor = 'black';
     })
 })
+}
+let canceled;
+
+function changeColorBlack(){
+    const gridDiv = document.querySelectorAll('#newDiv');
+    gridDiv.forEach(div =>{
+    div.addEventListener('mouseover', e => {
+    div.style.backgroundColor = 'black';
+    })
+})
+}
+
+function changeColorBlue(){
+    const gridDiv = document.querySelectorAll('#newDiv');
+    gridDiv.forEach(div =>{
+    div.addEventListener('mouseover', e => {
+    div.style.backgroundColor = 'blue';
+    })
+})
+}
+function changeColorRed(){
+    const gridDiv = document.querySelectorAll('#newDiv');
+    gridDiv.forEach(div =>{
+    div.addEventListener('mouseover', e => {
+    div.style.backgroundColor = 'red';
+    })
+})
+}
+
+function changeColorPurple(){
+    const gridDiv = document.querySelectorAll('#newDiv');
+    gridDiv.forEach(div =>{
+    div.addEventListener('mouseover', e => {
+    div.style.backgroundColor = 'purple';
+    })
+})
+}
+
+function changeColorYellow(){
+    const gridDiv = document.querySelectorAll('#newDiv');
+    gridDiv.forEach(div =>{
+    div.addEventListener('mouseover', e => {
+    div.style.backgroundColor = 'yellow';
+    })
+})
+}
+
+function changeColorOrange(){
+    const gridDiv = document.querySelectorAll('#newDiv');
+    gridDiv.forEach(div =>{
+    div.addEventListener('mouseover', e => {
+    div.style.backgroundColor = 'orange';
+    })
+})
+}
+
+function changeColorPink(){
+    const gridDiv = document.querySelectorAll('#newDiv');
+    gridDiv.forEach(div =>{
+    div.addEventListener('mouseover', e => {
+    div.style.backgroundColor = 'pink';
+    })
+})
+}
+
+function eraser(){
+    const gridDiv = document.querySelectorAll('#newDiv');
+    gridDiv.forEach(div =>{
+    div.addEventListener('mouseover', e => {
+    div.style.backgroundColor = 'white';
+    })
+})
+}
+
 
 function userChoice(){
     let validInput = false;
     while(validInput == false){
         userSelection = prompt('How wide would you like your canvas?');
-        if(isNaN(userSelection)){
+        if(userSelection === null){
+            return
+        }
+        else if(isNaN(userSelection)){
+            alert(`'${userSelection}' is not a number. please select a number from 1-18`)
             console.log('not a number');
             continue
         }
-        else if(userSelection > 100){
+        else if(userSelection < 1){
+            console.log('too small');
+            alert(`'${userSelection}' is too small. please select a number from 1-18`)
+            continue
+        }
+        else if(userSelection > 18){
             console.log('too big')
+            alert(`${userSelection} is too big. please select a number from 1-18`)
             continue
         }
         else {
-            console.log(userSelection)
-            changeGrid(userSelection)
+            validInput == true;
+            console.log(userSelection);
+            gridButton.remove();
+            createGrid(userSelection);
+            return
         }
     }
-}
-
-function changeGrid(){
-    
 }
 
 
